8c75e93f5355f37c79d245db146cde6cf753a516
joeyjerez <127454698+joeyjerez@users.noreply.github.com>
2023-10-22 21:53:58 -0300

    Se añade latitud y longitud

--- src/app/asistencias/asistencias.page.ts
+++ src/app/asistencias/asistencias.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-asistencias',
@@ -9,12 +10,17 @@ export class AsistenciasPage implements OnInit {
 
   dataProfe:string | null;
   userAlum: string | null;
+  latitud: number; // Se declara la variable latitud
+  longitud: number; // Se declara la variable longitud
 
   constructor() {
     this.dataProfe = localStorage.getItem("dataProfeCamera");
     
     let value = JSON.parse(localStorage.getItem("usuario")!);
     this.userAlum = value.nombre
+
+    this.latitud = 0; // Se inicializa latitud con un valor predeterminado
+    this.longitud = 0; // Se inicializa longitud con un valor predeterminado
     }
     
   mostrarInfo(){
@@ -28,5 +34,19 @@ export class AsistenciasPage implements OnInit {
   }
   ngOnInit() {
   }
+
+
+  //Creamos funcion para obtener ubicacion y mostrar las coordenadas
+  async obtenerUbicacion() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.mostrarCoordenadas(coordinates);
+  }
+
+  mostrarCoordenadas(coordenadas: any) {
+    this.latitud = coordenadas.coords.latitude;
+    this.longitud = coordenadas.coords.longitude;
+  }
+  
+  
 
 }
